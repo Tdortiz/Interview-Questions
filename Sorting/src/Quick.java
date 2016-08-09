3b9cb636310cb711a83d76f64bb8028dc68ac50b
Thomas Ortiz <tdortiz@ncsu.edu>
2016-08-08 20:53:16 -0400

    Further Setup

--- Sorting/src/Quick.java
+++ Sorting/src/Quick.java
@@ -1,4 +1,19 @@
 
 public class Quick {
-
+	
+	public static void main(String[] args){
+		int[] arr = setup();
+		
+	}
+	
+	public static int[] setup(){
+		int[] arr = new int[6];
+		arr[0] = 5;
+		arr[1] = 2;
+		arr[2] = 6;
+		arr[3] = 1;
+		arr[4] = 3;
+		arr[5] = 4;
+		return arr;
+	}
 }
