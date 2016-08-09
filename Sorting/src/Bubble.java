1e120f6ccebf908c8920fe73bfe0c277a0cee970
Thomas Ortiz <tdortiz@ncsu.edu>
2016-08-08 21:46:36 -0400

    Implemented Bubble Sort

--- Sorting/src/Bubble.java
+++ Sorting/src/Bubble.java
@@ -1,23 +1,50 @@
+import java.util.Arrays;
 
 public class Bubble {
 	public static void main(String[] args){
 		int[] arr = setup();
-		
+		sort(arr);
 	}
 	
-	public int[] sort(int[] arr){
+	public static void sort(int[] arr){
+		int iterationsLeft = arr.length;
 		
-		return null;
+		for(int i = 0; i < iterationsLeft; i++){
+			if(iterationsLeft <= 0 )
+				break;
+			
+			// Prevent sorted already sorted part of array
+			if( (i+1) >= iterationsLeft){
+				System.out.println(Arrays.toString(arr) + "\n");
+				iterationsLeft--;
+				i = 0;  
+			}
+			
+			int temp = 0;
+			int first = arr[i];
+			int second = arr[i+1];
+			
+			// Switch places
+			if( first > second){
+				temp = first;
+				arr[i] = second;
+				arr[i+1] = temp;
+				System.out.println(Arrays.toString(arr));
+			} else { 
+				continue; 
+			}
+		}
 	}
 	
 	public static int[] setup(){
 		int[] arr = new int[6];
-		arr[0] = 5;
-		arr[1] = 2;
-		arr[2] = 6;
-		arr[3] = 1;
-		arr[4] = 3;
-		arr[5] = 4;
+		arr[0] = 6;
+		arr[1] = 5;
+		arr[2] = 4;
+		arr[3] = 3;
+		arr[4] = 2;
+		arr[5] = 1;
 		return arr;
 	}
+	
 }
